79cdf61ccdae57bb51b95ed6e780dccb54fc59d3
Ryan Grunest <ryangrunest@geeko>
2024-10-04 22:38:14 +0200

    add comments and pseudocode

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@
 */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-void ClearConsole() {
+// clear the console
+void CLEAR_CONSOLE() {
   std::cout << "\x1B[2J\x1B[H";
+}
+
+// display doubles with 2 decimals
+void FIXED_FLOAT(double x) {
+  cout << std::fixed << std::setprecision(2) << x;
 }
 
 class Account {
@@ -18,44 +25,62 @@ class Account {
   int numYears;
 
   public:
-    Account() {
-      ClearConsole();
+    Account() { // initial constructor
+      CLEAR_CONSOLE();
 
-      std::cout << "Enter initial investment amount (Ex. 500): " << endl;
+      std::cout << "Enter initial investment amount (Ex. 500): " << endl; // get initial investment
       std::cin >> initialInvestment;
 
-      ClearConsole();
+      CLEAR_CONSOLE();
 
-      std::cout << "Enter monthly deposit (Ex. 50): " << endl;
+      std::cout << "Enter monthly deposit (Ex. 50): " << endl; // get monthly deposit
       std::cin >> monthlyDeposit;
 
-      ClearConsole();
+      CLEAR_CONSOLE();
 
-      std::cout << "Enter annual interest (Ex. 5.3): " << endl;
+      std::cout << "Enter annual interest (Ex. 5.3): " << endl; // get annual interest
       std::cin >> annualInterest;
       
-      ClearConsole();
+      CLEAR_CONSOLE();
 
-      std::cout << "Enter number of years (Ex. 5): " << endl;
+      std::cout << "Enter number of years (Ex. 5): " << endl; // get number of years of investments
       std::cin >> numYears;
+      
+      CLEAR_CONSOLE();
     };
 
-    void DisplayDataInput() {
-      ClearConsole();
+    void DisplayDataInput() { // display initial data
+      CLEAR_CONSOLE();
 
-      std::cout << "****************************" << endl;
-      std::cout << "******* Data Input *********" << endl;
-      std::cout << "Initial Investment Amount: " << initialInvestment << endl;
-      std::cout << "Monthly Deposit: " << monthlyDeposit << endl;
-      std::cout << "Annual Interest: " << annualInterest << endl;
+      std::cout << "********************************************************" << endl;
+      std::cout << "********************* Data Input ***********************" << endl;
+      std::cout << endl;
+
+      std::cout << "Initial Investment Amount: $";
+      FIXED_FLOAT(initialInvestment);
+      cout << endl;
+      cout << endl;
+
+      std::cout << "Monthly Deposit: $";
+      FIXED_FLOAT(monthlyDeposit);
+      cout << endl;
+      cout << endl;
+
+      std::cout << "Annual Interest: ";
+      FIXED_FLOAT(annualInterest);
+      cout << "%" << endl;
+      cout << endl;
+
       std::cout << "Number Of Years: " << numYears << endl;
-      std::cout << "Press Any Key To Continue . . ." << endl;
+      cout << endl;
+      
+      std::cout << "Press Enter / Return To Continue . . ." << endl;
     };
 
-    void DisplayChartWithoutInvestments() {
+    void DisplayChartWithoutInvestments() { // display chart without monthly investments
       int currentYear;
       int currentMonth;
-      double currentValue = initialInvestment;
+      double currentValue = initialInvestment; // set currentValue as initial investment to start
       double yearEndInterest;
 
       std::cout << "  Balance And Interest Without Additional Monthly Deposits  " << endl;
@@ -63,15 +88,15 @@ class Account {
       std::cout << "Year        Year End Balance        Year End Earned Interest" << endl;
       std::cout << "------------------------------------------------------------" << endl;
 
-      for (currentYear = 1; currentYear <= numYears; currentYear++) {
-        yearEndInterest = 0.00;
+      for (currentYear = 1; currentYear <= numYears; currentYear++) { // for each year in num years
+        yearEndInterest = 0.00; // reset interest
 
-        for (currentMonth = 1; currentMonth <= 12; currentMonth++) {
-          yearEndInterest = currentValue * ((annualInterest / 100) / 12);
-          currentValue += yearEndInterest;
+        for (currentMonth = 1; currentMonth <= 12; currentMonth++) { // for each month
+          yearEndInterest = currentValue * ((annualInterest / 100) / 12); // calculate interest for the month
+          currentValue += yearEndInterest; // calculate value for the year
         }
 
-        std::cout 
+        std::cout // display data
           << currentYear << "           " 
           << currentValue << "                     " 
           << yearEndInterest
@@ -81,10 +106,10 @@ class Account {
       std::cout << "============================================================" << endl;
     };
 
-    void DisplayChartWithInvestments() {
+    void DisplayChartWithInvestments() { // display chart with monthly investments
       int currentYear;
       int currentMonth;
-      double currentValue = initialInvestment;
+      double currentValue = initialInvestment; // set currentValue as initial investment to start
       double yearEndInterest;
       
       std::cout << "    Balance And Interest With Additional Monthly Deposits   " << endl;
@@ -93,33 +118,66 @@ class Account {
       std::cout << "------------------------------------------------------------" << endl;
       std::cout << "============================================================" << endl;
 
-      for (currentYear = 1; currentYear <= numYears; currentYear++) {
+      for (currentYear = 1; currentYear <= numYears; currentYear++) { // do same calculations for each year as above
         yearEndInterest = 0.00;
 
         for (currentMonth = 1; currentMonth <= 12; currentMonth++) {
           yearEndInterest = (currentValue + monthlyDeposit) * ((annualInterest / 100) / 12);
           currentValue += yearEndInterest;
-          currentValue += monthlyDeposit;
+          currentValue += monthlyDeposit; // except this time, add monthly deposit.
         }
 
-        std::cout 
+        std::cout // display data
           << currentYear << "           " 
           << currentValue << "                     " 
           << yearEndInterest
           << endl;
       }
+    };
+
+    int ValidateValues() { // validate values
+      // initial investment should be less than 100k and not negative
+      if (initialInvestment > 100000.00 || initialInvestment <= 0.001) {
+        std::cout << "Initial investment is too low or high. Try again." << endl;
+        return 1;
+      }
+
+      // same with monthly deposits
+      if (monthlyDeposit > 100000.00 || monthlyDeposit <= 0.001) {
+        std::cout << "Monthly deposit is too low or high. Try again." << endl;
+        return 1;
+      }
+
+      // annual interest should be less than 50%, and non negative.
+      if (annualInterest > 50.00 || annualInterest <= 0.001) {
+        std::cout << "Annual interest is too low or high. Try again." << endl;
+        return 1;
+      }
+
+      // number of years should be less than 100, and non negative.
+      if (numYears > 100 || numYears <= 1) {
+        std::cout << "Number of years is too low or high. Try again." << endl;
+        return 1;
+      }
+
+      return 0;
     }
 };
 
 int main() {
-  Account account; 
+  Account account; // init account
 
-  account.DisplayDataInput();
+  if (account.ValidateValues() == 0) { // make sure inputs are valid
+    account.DisplayDataInput(); // display input data
 
-  ClearConsole();
+    std::cin.ignore(2); // wait for user to hit enter
 
-  account.DisplayChartWithoutInvestments();
-  account.DisplayChartWithInvestments();
+    CLEAR_CONSOLE(); // clear console
 
-  return 0;
+    account.DisplayChartWithoutInvestments(); // display chart
+    account.DisplayChartWithInvestments(); // display chart
+    return 0; // return 0, all is well
+  } else {
+    return 1; // oh no, something went wrong
+  }
 }
---
+++ pseudocode.md
@@ -0,0 +1,28 @@
+## Outline Of Program
+
+Get user input
+
+- initial investment, monthly deposit, annual interest, number of years
+
+validate user input
+
+- if out of range, exit program
+
+display values entered
+
+wait for user input. after user presses enter...
+
+display chart without investments
+
+- for each year
+  - for each month
+    - add interest to current year's total
+  - display current year, current value, year end interest
+
+display chart with investments
+
+- for each year
+  - for each month
+    - add interest to current year's total
+    - add monthly deposit to current year's total
+  - display current year, current value, year end interest
